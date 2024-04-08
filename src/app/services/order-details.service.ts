a3bf12860e30aa58d5f38e3b53d125ea6450fbc8
Dipali Parab <dipaliparab182@gmail.com>
2024-04-08 10:36:31 +0200

    changes

--- src/app/services/order-details.service.ts
+++ src/app/services/order-details.service.ts
@@ -15,28 +15,42 @@ export class OrderDetailsService {
       foodName:"Paneer Grilled Sandwich",
       foodDetails:"Pan-fried Masala Paneer",
       foodPrice:200,
-      foodImg:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/wzfq7djolqxgdhghebbq"
+      foodImg:"/../../assets/img/paneer-sandwich.jpeg"
     },
     {
       id:2,
       foodName:"Veggie Supreme",
       foodDetails:"Onion| Green Capsicum|Mushroom |black olives , sweet corn , Red Paprika topped with Cheese",
       foodPrice:369,
-      foodImg:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/sgbobtbxlojbtdnr2m5k"
+      foodImg:"/../../assets/img/veggie-supreme.jpeg"
     },
     {
       id:3,
       foodName:"Paneer Burger",
       foodDetails:"panner",
       foodPrice:149,
-      foodImg:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/xbeqlsck3p0kei53to7k"
+      foodImg:"/../../assets/img/paneer-burger.jpeg"
     },
     {
       id:4,
       foodName:"Veg Masala Roll In Naan",
       foodDetails:"A homely mix of mashed potato and veggies, seasoned with Indian spices. A filling sure to take you back to mom's kitchen.r",
       foodPrice:140,
-      foodImg:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/l2ng6gtge30sqaafqng7"
+      foodImg:"/../../assets/img/veg-roll.jpeg"
+    },
+    {
+      id:5,
+      foodName:"Indulgence Brownie",
+      foodDetails:"(Eggless) Indulge in richly decadent chocolate brownie crafted with love & topped with bitter-sweet chocolate that provides ultra-rich chocolate experience.",
+      foodPrice:105,
+      foodImg:"/../../assets/img/brownie.jpeg"
+    },
+    {
+      id:6,
+      foodName:"Oreo Cheesecake Ice Cream",
+      foodDetails:"Oreo ice cream",
+      foodPrice:219,
+      foodImg:"/../../assets/img/oreo-cheesecake.jpeg"
     }
   ]
 }
